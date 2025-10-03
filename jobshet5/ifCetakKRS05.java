077603ae7431ce3272369e3a29a8b9f4800db636
anandarosyid <rosyidananda28@gmail.com>
2025-10-03 19:34:11 +0700

    perobaan 1

---
+++ jobshet5/ifCetakKRS05.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ifCetakKRS05 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("====Cetak KRS SIAKAD ===== ");
+       System.out.print("Apkah UKT sudah lunas ? (true/false) : "); 
+         boolean uktLunas = input.nextBoolean();
+            if (uktLunas) {
+                System.out.println("Pembayarn UKT terverifikasi");
+                System.out.println("silahkan cetak KRS dan mita tanda tangan DPA");
+            } 
+            }
+    
+         
+
+       
+    }

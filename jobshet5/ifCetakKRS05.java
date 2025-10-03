4fe934f2404a54806fcdc6674265f43e4083e2fd
anandarosyid <rosyidananda28@gmail.com>
2025-10-04 00:29:06 +0700

    percobaan 2

--- jobshet5/ifCetakKRS05.java
+++ jobshet5/ifCetakKRS05.java
@@ -4,7 +4,7 @@ public class ifCetakKRS05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("====Cetak KRS SIAKAD ===== ");
+        System.out.println("====Cetak KRS SIAKAD ====== ");
        System.out.print("Apkah UKT sudah lunas ? (true/false) : "); 
          boolean uktLunas = sc.nextBoolean();
             if (uktLunas) {
--- jobshet5/switchCetakKRS05.java
+++ jobshet5/switchCetakKRS05.java
@@ -1,42 +1,40 @@
-import java.util.Scanner;
 public class switchCetakKRS05 {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("===== Cetak KRS Siakad =====");
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("===== Cetak KRS Siakad ======");
         System.out.print("masukan semeester saat ini  : ");
         int semester = sc.nextInt();
-
+        
         switch (semester) {
             case 1:
-                System.out.println("KRS semester 1 tampilkan");
+                System.out.println("KRS semester 1 ditampilkan");
                 break;
             case 2:
-                System.out.println("KRS semester 2 tampilkan");         
+                System.out.println("KRS semester 2 ditampilkan");
                 break;
             case 3:
-                System.out.println("KRS semester 3 tampilkan"); 
+                System.out.println("KRS semester 3 ditampilkan");
                 break;
             case 4:
-                System.out.println("KRS semester 4 tampilkan");
+                System.out.println("KRS semester 4 ditampilkan");
                 break;
             case 5:
-                System.out.println("KRS semester 5 tampilkan");
+                System.out.println("KRS semester 5 ditampilkan");
                 break;
             case 6:
-                System.out.println("KRS semester 6 tampilkan");
+                System.out.println("KRS semester 6 ditampilkan");
                 break;
             case 7:
-                System.out.println("KRS semester 7 tampilkan");
+                System.out.println("KRS semester 7 ditampilkan");
                 break;
             case 8:
-                System.out.println("KRS semester 8 tampilkan");
+                System.out.println("KRS semester 8 ditampilkan");
                 break;
             default:
-                System.out.println("semester tidak valid");
+                System.out.println("Semester tidak valid");
                 break;
         }
-
-         }
+        sc.close();
         
-    }
+    }   
+}

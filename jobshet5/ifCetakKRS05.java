e272c25e9c9ed2a20485022565db950fb911ee13
anandarosyid <rosyidananda28@gmail.com>
2025-10-04 10:17:49 +0700

    Tugas 1

--- jobshet5/ifCetakKRS05.java
+++ jobshet5/ifCetakKRS05.java
@@ -2,16 +2,20 @@ import java.util.*;
 public class ifCetakKRS05 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner Sc = new Scanner(System.in);
 
         System.out.println("===== Cetak KRS SIAKAD ====== ");
        System.out.print("Apkah UKT sudah lunas ? (true/false) : "); 
-         boolean uktLunas = sc.nextBoolean();
-            if (uktLunas) {
-                System.out.println("Pembayarn UKT terverifikasi");
-                System.out.println("silahkan cetak KRS dan mita tanda tangan DPA");
-            } else {
-                System.out.println("“Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-            } 
+         boolean uktLunas = Sc.nextBoolean();
+
+         String hasil = (uktLunas) ? "Pembayaran UKT terverifikasi" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+           // if (uktLunas) {
+             //   System.out.println("Pembayarn UKT terverifikasi");
+               // System.out.println("silahkan cetak KRS dan mita tanda tangan DPA");
+          //  } else {
+              //  System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+            System.out.println(hasil);
+          Sc.close();
+          //  } 
         } 
     }
